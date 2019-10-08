fbe289f045a1a23441203096dfb281e08260f7fa
Song Li <ghosty.lee.1984@gmail.com>
2019-10-07 23:32:08 -0400

    home.nix: add direnv git bat alacritty

---
+++ home.nix
@@ -0,0 +1,85 @@
+{ pkgs, ... }:
+
+{
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "ansi-dark";
+    };
+  };
+  programs.git = {
+    enable = true;
+    userName = "Song Li";
+    userEmail = "ghosty.lee.1984@gmail.com";
+    aliases = {
+      co = "checkout";
+      cob = "checkout -b";
+      br = "branch";
+      st = "status";
+      log-fancy = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset' --abbrev-commit --date=relative";
+      log-nice = "log --graph --decorate --pretty=oneline --abbrev-commit";
+      master = "checkout master";
+      cm = "commit -m";
+      cam = "commit -a -m";
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      scrolling = {
+        history = 10000;
+        multiplier = 3;
+        faux_multiplier = 3;
+        auto_scroll = false;
+      };
+      tabspaces = 4;
+      font = {
+        normal = {
+          family = "RobotoMono Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "RobotoMono Nerd Font";
+          style = "Regular";
+        };
+        italic = {
+          family = "RobotoMono Nerd Font";
+          style = "Regular";
+        };
+        size = 14;
+      };
+      colors = {
+        primary.background = "0x282828";
+        primary.foreground = "0xebdbb2";
+
+        normal = {
+          black   = "0x282828";
+          red     = "0xcc241d";
+          green   = "0x98971a";
+          yellow  = "0xd79921";
+          blue    = "0x458588";
+          magenta = "0xb16286";
+          cyan    = "0x689d6a";
+          white   = "0xa89984";
+        };
+
+        bright = {
+          black   = "0x928374";
+          red     = "0xfb4934";
+          green   = "0xb8bb26";
+          yellow  = "0xfabd2f";
+          blue    = "0x83a598";
+          magenta = "0xd3869b";
+          cyan    = "0x8ec07c";
+          white   = "0xebdbb2";
+        };
+      };
+      live_config_reload = true;
+    };
+  };
+}
