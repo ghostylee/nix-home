c3454150226553359b5e9575b8e1294fb5858101
Song Li <ghosty.lee.1984@gmail.com>
2020-05-01 01:30:10 -0400

    zsh: better history search

--- home.nix
+++ home.nix
@@ -174,6 +174,11 @@
     initExtra =
       ''
         zstyle ':completion:*' menu select
+        autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
+        zle -N up-line-or-beginning-search
+        zle -N down-line-or-beginning-search
+        bindkey "^[[A" up-line-or-beginning-search
+        bindkey "^[[B" down-line-or-beginning-search
       '';
     plugins = [
       {
