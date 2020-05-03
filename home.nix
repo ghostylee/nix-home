4698cde1cc48e7e3f3f27a3f5c8c2e927f0f75fd
Song Li <ghosty.lee.1984@gmail.com>
2020-05-03 00:30:54 -0400

    add bspwm sxhkd configuration

--- home.nix
+++ home.nix
@@ -172,7 +172,14 @@
       POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=0;
       POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND=1;
     };
-    profileExtra = ". $HOME/.nix-profile/etc/profile.d/nix.sh";
+    profileExtra =
+    ''
+    source ~/.nix-profile/etc/profile.d/nix.sh
+    '';
+    initExtraBeforeCompInit =
+    ''
+    source /etc/profile
+    '';
     initExtra =
       ''
         zstyle ':completion:*' menu select
@@ -282,6 +289,58 @@
         };
       };
       live_config_reload = true;
+    };
+  };
+
+  programs.rofi = {
+    enable = true;
+  };
+
+  programs.firefox = {
+    enable = true;
+  };
+
+  xsession.enable = true;
+  xsession.windowManager.bspwm = {
+    enable = true;
+    monitors = {
+      "" = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" ];
+    };
+    settings = {
+      border_width = 2;
+      window_gap = 12;
+      split_ratio = 0.52;
+      borderless_monocle = true;
+      gapless_monocle = true;
+    };
+  };
+
+  services.sxhkd = {
+    enable = true;
+    keybindings = {
+      "super + Return" = "alacritty";
+      "super + @space" = "rofi -show run";
+      "super + alt + {q,r}" = "bspc {quit,wm -r}";
+      "super + {_,shift + }w" = "bspc node -{c,k}";
+      "super + m" = "bspc desktop -l next";
+      "super + y" = "bspc node newest.marked.local -n newest.!automatic.local";
+      "super + g" = "bspc node -s biggest";
+      "super + {t,shift + t,s,f}" = "bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
+      "super + ctrl + {m,x,y,z}" = "bspc node -g {marked,locked,sticky,private}";
+      "super + {_,shift + }{h,j,k,l}" = "bspc node -{f,s} {west,south,north,east}";
+      "super + {p,b,comma,period}" = "bspc node -f @{parent,brother,first,second}";
+      "super + {_,shift + }c" = "bspc node -f {next,prev}.local";
+      "super + bracket{left,right}" = "bspc desktop -f {prev,next}.local";
+      "super + {grave,Tab}" = "bspc {node,desktop} -f last";
+      "super + {o,i}" = "bspc wm -h off; bspc node {older,newer} -f; bspc wm -h on";
+      "super + {_,shift + }{1-9,0}" = "bspc {desktop -f,node -d} '^{1-9,10}'";
+      "super + ctrl + {h,j,k,l}" = "bspc node -p {west,south,north,east}";
+      "super + ctrl + {1-9}" = "bspc node -o 0.{1-9}";
+      "super + ctrl + space" = "bspc node -p cancel";
+      "super + ctrl + shift + space" = "bspc query -N -d | xargs -I id -n 1 bspc node id -p cancel";
+      "super + alt + {h,j,k,l}" = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
+      "super + alt + shift + {h,j,k,l}" = "bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}";
+      "super + {Left,Down,Up,Right}" = "bspc node -v {-20 0,0 20,0 -20,20 0}";
     };
   };
 }
