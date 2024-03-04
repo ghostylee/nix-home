430e1d2ef90e79c6c8938009b8524f41815b3575
Song Li <ghosty.lee.1984@gmail.com>
2024-03-03 22:13:17 -0500

    remove nvim-gps and add wl-clipboard and tree-sitter

--- home.nix
+++ home.nix
@@ -47,6 +47,8 @@
     gnome.eog
     libva-utils
     tradingview
+    wl-clipboard
+    tree-sitter
   ];
   # }}}
   # home-manager {{{
@@ -288,23 +290,6 @@
 
       require'colorizer'.setup()
 
-      require("nvim-gps").setup({
-        icons = {
-          ["class-name"] = ' ',      -- Classes and class-like objects
-          ["function-name"] = ' ',   -- Functions
-          ["method-name"] = ' ',     -- Methods (functions inside class-like objects)
-          ["container-name"] = '⛶ ',  -- Containers (example: lua tables)
-          ["tag-name"] = '炙'         -- Tags (example: html tags)
-        },
-        -- Disable any languages individually over here
-        -- Any language not disabled here is enabled by default
-        languages = {
-          -- ["bash"] = false,
-          -- ["go"] = false,
-        },
-        separator = ' > ',
-      })
-
       require("symbols-outline").setup()
       EOF
 
@@ -416,7 +401,6 @@
       numb-nvim
       symbols-outline-nvim
       nvim-colorizer-lua
-      nvim-gps
       markdown-preview-nvim
     ];
   };
