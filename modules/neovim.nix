66155c3136a3b75fa2b1d8fa1f5ccbc45e897d7f
Song Li <ghosty.lee.1984@gmail.com>
2026-01-11 15:33:51 -0500

    neovim: change nvim-treesitter setup

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -168,7 +168,7 @@
       -- Defines a read-write directory for treesitters in nvim's cache dir
       local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
       vim.fn.mkdir(parser_install_dir, "p")
-      require'nvim-treesitter.configs'.setup {
+      require'nvim-treesitter'.setup {
         parser_install_dir = parser_install_dir,
         ensure_installed = "",
         highlight = {
